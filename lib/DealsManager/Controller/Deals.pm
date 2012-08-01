57fb8a110e8586f89ffa4ae12f4bacc132854b55
Gil Magno <gilmagno@gmail.com>
2012-08-01 12:45:42 -0300

    Code formatting and ponctuation

--- lib/DealsManager/Controller/Deals.pm
+++ lib/DealsManager/Controller/Deals.pm
@@ -8,27 +8,23 @@ BEGIN { extends 'Catalyst::Controller::HTML::FormFu'; }
 sub base :Chained('/') PathPart('deals') CaptureArgs(0) {
     my ( $self, $c ) = @_;
 
-    $c->stash->{resulset} = $c->model('DB::Deal');
+    $c->stash->{deals_rs} = $c->model('DB::Deal');
 }
 
 sub object :Chained('base') PathPart('') CaptureArgs(1) {
     my ( $self, $c, $id ) = @_;
 
-    $c->stash->{object} =
-      $c->stash->{resulset}->search_rs({ 'me.id' => $id });
+    $c->stash->{deals_rs} =
+      $c->stash->{deals_rs}->search_rs({ 'me.id' => $id });
 }
 
 sub index :Chained('base') PathPart('') Args(0) {
     my ( $self, $c ) = @_;
 
-    my $deals_rs = $c->stash->{resulset}
-      ->search(
-               undef,
-               {
-                prefetch => ['responsible', 'contact'],
-                order_by => { -asc => 'me.id' }
-               }
-              );
+    my $deals_rs =
+      $c->stash->{deals_rs}->search( undef,
+                                     { prefetch => ['responsible', 'contact'],
+                                       order_by => { -asc => 'me.id' } } );
 
     $c->stash->{deals} = [ $deals_rs->all ];
 
@@ -39,16 +35,16 @@ sub index :Chained('base') PathPart('') Args(0) {
 sub show :Chained('object') PathPart('') Args(0) FormConfig('deals/notes/form.yml') {
     my ( $self, $c ) = @_;
 
-    my $deal = $c->stash->{object}
-      ->search_rs(undef,
-                  { prefetch => [ 'responsible', 'contact', {'notes' => 'user' } ],
-                    order_by => { -desc => 'notes.id' } } )
+    my $deal = $c->stash->{deals_rs}
+      ->search_rs( undef,
+                   { prefetch => [ 'responsible', 'contact', {'notes' => 'user' } ],
+                     order_by => { -desc => 'notes.id' } } )
         ->first;
 
     $c->stash->{form}->action("/deals/" . $deal->id . "/notes/create");
     $c->stash->{form}->default_values({ deal_id => $deal->id, user_id => $c->user->id });
 
-    $c->stash->{object} = $deal;
+    $c->stash->{deal} = $deal;
     use DateTime::Duration::Fuzzy qw/time_ago/;
     $c->stash->{time_ago} = sub { time_ago($_[0]); };
 
@@ -64,10 +60,9 @@ sub create :Chained('base') PathPart('create') Args(0) FormConfig('deals/form.ym
     if ( $form->submitted_and_valid ) {
         use DateTime;
         my $datetime_now = DateTime->now;
-        my $new_deal = $c->stash->{resulset}->new_result({
-                                                          created => $datetime_now,
-                                                          updated => $datetime_now
-                                                         });
+        my $new_deal =
+          $c->stash->{deals_rs}->new_result( { created => $datetime_now,
+                                              updated => $datetime_now } );
         $form->model->update( $new_deal );
 
         my $dashboard_item = { type => 'deal_created',
@@ -77,7 +72,7 @@ sub create :Chained('base') PathPart('create') Args(0) FormConfig('deals/form.ym
 
         my $dashboard_rs = $c->model('DB::Dashboard')->create( $dashboard_item );
 
-        $c->flash->{success} = 'Deal <b>' . $new_deal->name  . '</b> created';
+        $c->flash->{success} = 'Deal <b>' . $new_deal->name  . '</b> created.';
         $c->res->redirect('/deals');
     }
 }
@@ -88,9 +83,7 @@ sub edit :Chained('object') PathPart('edit') Args(0) FormConfig('deals/form.yml'
     my $form = $c->stash->{form};
 
     if ( $form->submitted_and_valid ) {
-        use DateTime;
-
-        my $deal = $c->stash->{object}->first;
+        my $deal = $c->stash->{deals_rs}->first;
 
         my $fields_and_labels =
           { status => 'Status',
@@ -104,19 +97,17 @@ sub edit :Chained('object') PathPart('edit') Args(0) FormConfig('deals/form.yml'
 
         for my $field (keys %$fields_and_labels) {
             unless ($form->param_value($field) eq $deal->$field) {
-
-                my $chunk = {
-                             name => $field,
-                             original => $deal->$field,
-                             modified => $form->param_value($field),
-                             label => $fields_and_labels->{$field}
-                            };
+                my $chunk = { name => $field,
+                              original => $deal->$field,
+                              modified => $form->param_value($field),
+                              label => $fields_and_labels->{$field} };
 
                 push @modified_fields, $chunk;
             }
         }
 
         use JSON;
+        use DateTime;
 
         my $changes_json = encode_json \@modified_fields;
 
@@ -133,19 +124,19 @@ sub edit :Chained('object') PathPart('edit') Args(0) FormConfig('deals/form.yml'
 
         my $dashboard_rs = $c->model('DB::Dashboard')->create( $dashboard_item );
 
-        $c->flash->{success} = 'Deal <b>' . $deal->name . '</b> edited';
+        $c->flash->{success} = 'Deal <b>' . $deal->name . '</b> edited.';
         $c->res->redirect('/deals');
     }
     else {
-        $form->model->default_values( $c->stash->{object}->first );
+        $form->model->default_values( $c->stash->{deals_rs}->first );
     }
 }
 
 sub delete :Chained('object') PathPart('delete') Args(0) {
     my ( $self, $c ) = @_;
 
-    my $deal = $c->stash->{object}->first;
-    $c->flash->{error} = 'Deal <b>' . $deal->name . '</b> deleted';
+    my $deal = $c->stash->{deals_rs}->first;
+    $c->flash->{error} = 'Deal <b>' . $deal->name . '</b> deleted.';
     $deal->delete;
 
     $c->res->redirect('/deals');
